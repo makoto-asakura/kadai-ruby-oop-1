cd6d49d15d5a16e0e3ed691e040b8fb4fce4f1eb
makoto.asakura <makoto@asakula.com>
2021-12-10 05:45:11 +0000

    modify

--- team.rb
+++ team.rb
@@ -14,26 +14,26 @@ class Team
   
   # インスタンスが持つメソッド（処理）
   def calc_win_rate
-    return self.win.to_f / (self.win.to_f + self.lose.to_f)
+    self.win.to_f / (self.win.to_f + self.lose.to_f)
   end
   
   def show_team_result
-    puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}負#{self.draw}分、勝率は#{calc_win_rate}です。"
+    puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}負 #{self.draw}分、 勝率は #{calc_win_rate}です。"
   end
   
   #インスタンスの生成
-  Giants=Team.new("Giants",67,45,8)
-  Tigers=Team.new("Tigers",60,53,7)
-  Dragons=Team.new("Dragons",60,55,5)
-  BayStars=Team.new("Baystars",56,58,6)
-  Carp=Team.new("Carp",52,56,12)
-  Swallows=Team.new("Swallows",41,69,10)
+  giants=Team.new("Giants",67,45,8)
+  tigers=Team.new("Tigers",60,53,7)
+  dragons=Team.new("Dragons",60,55,5)
+  bay_stars=Team.new("Baystars",56,58,6)
+  carp=Team.new("Carp",52,56,12)
+  swallows=Team.new("Swallows",41,69,10)
   
   #インスタンスの使用
-  Giants.show_team_result
-  Tigers.show_team_result
-  Dragons.show_team_result
-  BayStars.show_team_result
-  Carp.show_team_result
-  Swallows.show_team_result
+  giants.show_team_result
+  tigers.show_team_result
+  dragons.show_team_result
+  bay_stars.show_team_result
+  carp.show_team_result
+  swallows.show_team_result
 end
